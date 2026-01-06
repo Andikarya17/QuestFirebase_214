a8b9eec95d6f60b52f1bdb183697a974e2ad4d68
Andikarya17 <andikaryaputra17@gmail.com>
2026-01-06 09:58:14 +0700

    feat: Implement App composable
    Implement the `DataSiswaApp` composable which sets up the `HostNavigasi`. This serves as the main entry point for the application's UI.

--- app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
@@ -6,16 +6,18 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.myfirebase.view.EntrySiswaScreen
+import com.example.myfirebase.view.HomeScreen
 import com.example.myfirebase.view.route.DestinasiDetail
 import com.example.myfirebase.view.route.DestinasiEntry
 import com.example.myfirebase.view.route.DestinasiHome
 
 @Composable
 fun DataSiswaApp(navController: NavHostController = rememberNavController(),
-                 modifier: Modifier
-){
+                 modifier: Modifier){
     HostNavigasi(navController = navController)
 }
+
 @Composable
 fun HostNavigasi(
     navController: NavHostController,
