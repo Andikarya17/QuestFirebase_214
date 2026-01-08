3bbe63bfde571a7c4511c25c10e2bf2c4ed0e3d2
Andikarya17 <andikaryaputra17@gmail.com>
2026-01-08 07:52:20 +0700

    feat: Add DetailSiswaScreen Composable
    Add the initial `DetailSiswaScreen` composable function. This screen is intended to display student details and will include navigation to an edit screen and a way to navigate back. The function currently includes parameters for `navigateToEdit`, `navigateBack`, and a `DetailViewModel`.

---
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -0,0 +1,16 @@
+package com.example.myfirebase.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEdit: (Long) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
