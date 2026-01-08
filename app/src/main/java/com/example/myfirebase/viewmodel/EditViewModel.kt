6d0d12093226281f9a3b8a63bcec6e8a47f62402
Andikarya17 <andikaryaputra17@gmail.com>
2026-01-08 07:46:50 +0700

    feat(EditViewModel): Add UI state and ID handling
    Initialize `uiStateSiswa` using `mutableStateOf` to manage the UI state for editing a student.
    
    Also, retrieve `idSiswa` from the `SavedStateHandle`. This ensures the ViewModel can identify which student is being edited based on the navigation argument.

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -1,10 +1,22 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.myfirebase.modeldata.UIStateSiswa
 import com.example.myfirebase.repositori.RepositorySiswa
+import com.example.myfirebase.view.route.DestinasiDetail
 
 class EditViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositorySiswa: RepositorySiswa
-) : ViewModel()
+) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+}
