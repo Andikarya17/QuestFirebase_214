4aa80ef21d16788efe349f347435e8f8079a4949
Andikarya17 <andikaryaputra17@gmail.com>
2026-01-08 07:54:34 +0700

    feat: Add DetailViewModel
    Add the `DetailViewModel` class to manage the UI state for the detail screen.
    
    This ViewModel retrieves a single student's data using `idSiswa` from `SavedStateHandle` and exposes the data fetching status through `statusUIDetail`.

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -36,3 +36,17 @@ class DetailViewModel(
     init {
         getSatuSiswa()
     }
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(
+                    satusiswa = repositorySiswa.getSatuSiswa(idSiswa)
+                )
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: Exception) {
+                StatusUIDetail.Error
+            }
+        }
+    }
