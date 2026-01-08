a194701b673bd20c919805bb812e5dba2127dae9
Andikarya17 <andikaryaputra17@gmail.com>
2026-01-08 07:43:31 +0700

    feat: Implement TopAppBar in HalamanEdit
    Implement the `Scaffold` layout in the `HalamanEdit` composable. This adds a `SiswaTopAppBar` with a title from `DestinasiEdit.titleRes` and enables back navigation.

--- app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
@@ -1,8 +1,11 @@
 package com.example.myfirebase.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.myfirebase.view.route.DestinasiEdit
 import com.example.myfirebase.viewmodel.PenyediaViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -12,4 +15,15 @@ fun EditSiswaScreen(
     onNavigateUp: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {}
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    )
+}
