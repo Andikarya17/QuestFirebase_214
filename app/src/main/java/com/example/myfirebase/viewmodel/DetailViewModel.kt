feef5878a6e1de31b130f49b94c134a15c0c477c
Andikarya17 <andikaryaputra17@gmail.com>
2026-01-08 07:53:54 +0700

    feat: Add StatusUIDetail sealed interface
    Add the `StatusUIDetail` sealed interface to represent the different states of the detail view UI.
    
    This interface includes the following states:
    - `Success`: Represents a successful data fetch, containing the student data (`Siswa?`).
    - `Error`: Represents an error state.
    - `Loading`: Represents a loading state while data is being fetched.

---
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.myfirebase.viewmodel
+
+import kotlinx.serialization.InternalSerializationApi
+
+@file:OptIn(InternalSerializationApi::class)
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.Siswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
