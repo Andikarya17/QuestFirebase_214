6dea9315b57f94d256f103132e753c7c86c1d161
Andikarya17 <andikaryaputra17@gmail.com>
2026-01-08 08:16:18 +0700

    feat: Add initializers for DetailViewModel and EditViewModel
    Add ViewModel initializers to the `PenyediaViewModel.Factory`.
    
    - Add an initializer for `DetailViewModel`, providing it with a `SavedStateHandle` and the `repositorySiswa`.
    - Add an initializer for `EditViewModel`, also providing it with a `SavedStateHandle` and the `repositorySiswa`.

--- app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.myfirebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -25,6 +26,19 @@ object PenyediaViewModel {
 
         initializer {
             EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
                 aplikasiDataSiswa().container.repositorySiswa
             )
         }
